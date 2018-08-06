8b0f2b55bdf86eaca5dddb9b9e06291cb7c7f2b1
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-07 00:38:11 +0200

    doubling with fraction

--- week-02/day-01/01 - doubling.ts
+++ week-02/day-01/01 - doubling.ts
@@ -3,6 +3,7 @@ export { }
 // -  Create a variable named `baseNum` and assign the value `123` to it
 // -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
 // -  Log the result of `doubling(baseNum)` to the console
+//
 
 var baseNum: number = 123;
 var multiplier: number = 2;
