0e9932324715566cb52ee0d7296d1068fd412008
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-07 08:35:26 +0200

    in line and in column printing parameters

--- week-02/day-01/06 - print params.ts
+++ week-02/day-01/06 - print params.ts
@@ -1,6 +1,7 @@
 // -  Create a function called `printParams`
 //    which logs to the console the input parameters
 //    (can have multiple number of arguments)
+//print
 export { }
 
 function printParamsFirst (...inputs:any[]) {
