78428869e6125dfa965d543964ebd1070657a565
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-07 08:45:00 +0200

    using two variables

--- week-02/day-01/09 - sum elements.ts
+++ week-02/day-01/09 - sum elements.ts
@@ -1,5 +1,6 @@
 // -  Create a variable named `r` with the following content: `[54, 23, 66, 12]`
 // -  Print the sum of the second and the third element
+//
 
 var r = [54, 23, 66, 12];
 var selectedElementOne: number = 2;
