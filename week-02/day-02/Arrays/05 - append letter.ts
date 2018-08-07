1b4d4fb5a2fb2025ec30b9938f5a2e2640abaabd
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-07 16:06:11 +0200

    A added with map

---
+++ week-02/day-02/Arrays/05 - append letter.ts
@@ -0,0 +1,14 @@
+'use strict';
+export { };
+// Create a function called "appendA()" that adds "a" to every string in the far list.
+// The parameter should be a list.
+let far: string[] = ['bo', 'anacond', 'koal', 'pand', 'zebr'];
+let aToAdd: string = 'a';
+function appendA() {
+    let output = far.map(x => x + aToAdd);
+
+    console.log(output);
+}
+appendA();
+// The output should be: 'boa', 'anaconda', 'koala', 'panda', 'zebra'
+export = appendA;
