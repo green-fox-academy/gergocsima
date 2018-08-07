a024cf5761881bee2597f42319601dbcb100cff8
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-07 08:45:29 +0200

    doubling with function

--- week-02/day-01/01 - doubling.ts
+++ week-02/day-01/01 - doubling.ts
@@ -3,7 +3,6 @@ export { }
 // -  Create a variable named `baseNum` and assign the value `123` to it
 // -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
 // -  Log the result of `doubling(baseNum)` to the console
-//variables
 
 var baseNum: number = 123;
 var multiplier: number = 2;
