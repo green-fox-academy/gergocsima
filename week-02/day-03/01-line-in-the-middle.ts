e31ee5183e43c3a3b64a8d7e267125116206ee3e
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-08 10:40:06 +0200

    ts created

---
+++ week-02/day-03/01-line-in-the-middle.ts
@@ -0,0 +1,9 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+ctx.fillRect(100, 100, 100, 100);
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// draw a red horizontal line to the canvas' middle.
+// draw a green vertical line to the canvas' middle.
