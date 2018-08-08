ebbe15412786d2e2d8618261a271c0f3d7980122
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-08 14:38:58 +0200

    simple solution

---
+++ week-02/day-03/03-diagonal.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Draw the canvas' diagonals.
+// If it starts from the upper-left corner it should be green, otherwise it should be red.
+
+//draw diagonal
+
+let width: number = 600;
+let heigth: number = 400;
+let diagonalNumber: number = 0;
+
+ctx.beginPath(); // define the place where to start to draw the rectangle
+ctx.strokeStyle = "red";
+ctx.moveTo(0,0);
+ctx.lineTo(width,heigth);
+ctx.stroke();
+ctx.closePath();
+ctx.beginPath();
+ctx.moveTo(width,0);
+ctx.strokeStyle = "green";
+ctx.lineTo(0,heigth);
+ctx.stroke();
+ctx.closePath();
