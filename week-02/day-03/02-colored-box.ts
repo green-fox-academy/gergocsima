27a0bf38d3f5b1e01908802b43ee4597672c439f
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-08 13:44:14 +0200

    shorter solution added, continious line

--- week-02/day-03/02-colored-box.ts
+++ week-02/day-03/02-colored-box.ts
@@ -10,34 +10,60 @@ let sideB: number = 200;
 let placeToDrawX: number = 60; // x coordinate of the start point
 let placeToDrawY: number = 100; // y coordinate of the start point
 
-
 ctx.beginPath(); // define the place where to start to draw the rectangle
 ctx.strokeStyle = "red";
-ctx.moveTo(placeToDrawX,placeToDrawY);
-ctx.lineTo(placeToDrawX+sideA,placeToDrawY);
+ctx.moveTo(placeToDrawX, placeToDrawY);
+ctx.lineTo(placeToDrawX + sideA, placeToDrawY);
 ctx.stroke();
 ctx.closePath();
 
 ctx.beginPath(); // define the place where to start to draw the rectangle
 ctx.strokeStyle = "blue";
-ctx.moveTo(placeToDrawX+sideA,placeToDrawY);
-ctx.lineTo(placeToDrawX+sideA,placeToDrawY+sideB);
+ctx.moveTo(placeToDrawX + sideA, placeToDrawY);
+ctx.lineTo(placeToDrawX + sideA, placeToDrawY + sideB);
 ctx.stroke();
 ctx.closePath();
 
 ctx.beginPath(); // define the place where to start to draw the rectangle
 ctx.strokeStyle = "green";
-ctx.moveTo(placeToDrawX,placeToDrawY+sideB);
-ctx.lineTo(placeToDrawX+sideA,placeToDrawY+sideB);
+ctx.moveTo(placeToDrawX, placeToDrawY + sideB);
+ctx.lineTo(placeToDrawX + sideA, placeToDrawY + sideB);
 ctx.stroke();
 ctx.closePath();
 
 
 ctx.beginPath(); // define the place where to start to draw the rectangle
 ctx.strokeStyle = "orange";
-ctx.moveTo(placeToDrawX,placeToDrawY);
-ctx.lineTo(placeToDrawX,placeToDrawY+sideB);
+ctx.moveTo(placeToDrawX, placeToDrawY);
+ctx.lineTo(placeToDrawX, placeToDrawY + sideB);
 ctx.stroke();
 ctx.closePath();
+/*
+//SHORTER PROGRAM
+let sideC: number = 10;
+let sideD: number = 200;
+let placeToDrawX: number = 30; // x coordinate of the start point
+let placeToDrawY: number = 10; // y coordinate of the start point
+
+ctx.beginPath(); // define the place where to start to draw the rectangle
+ctx.strokeStyle = "red";
+ctx.moveTo(placeToDrawX, placeToDrawY);
+ctx.lineTo(placeToDrawX + sideC, placeToDrawY);
+//ctx.stroke();
+
+// define the place where to start to draw the rectangle
+ctx.strokeStyle = "blue";
+ctx.lineTo(placeToDrawX + sideC, placeToDrawY + sideD);
+ctx.stroke();
+
+// define the place where to start to draw the rectangle
+ctx.strokeStyle = "green";
+ctx.lineTo(placeToDrawX, placeToDrawY + sideD);
+ctx.stroke();
 
 
+// define the place where to start to draw the rectangle
+ctx.strokeStyle = "orange";
+ctx.lineTo(placeToDrawX, placeToDrawY);
+ctx.stroke();
+ctx.closePath();
