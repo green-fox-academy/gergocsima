a0ed115c8062dfbdd1aaca12ef9f79eeb108e929
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-09 12:31:57 +0200

    function for squares with random color to the center

---
+++ week-02/day-03/09-center-box-function.ts
@@ -0,0 +1,29 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Create a square drawing function that takes 1 parameter:
+// The square size
+// and draws a square of that size to the center of the canvas.
+// Draw 3 squares with that function.
+// Avoid code duplication.
+
+//parameter given
+
+let size: number = 80;
+
+//function drawing square
+
+function squareDrawing(x: number) {
+    //    random color generator
+    ctx.fillStyle = 'rgb(' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ',' + Math.floor(Math.random() * 255) + ')';
+    ctx.fillRect(canvas.width / 2 - size / 2, canvas.height / 2 - size / 2, x, x);
+}
+
+
+for (let i: number = 0; i < 3; i++) {
+    squareDrawing(size);
+    size=size-10;
+}
