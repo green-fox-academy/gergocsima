87ae83f3b5ffd0436c5a2178c0ff859f3f101588
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-09 11:39:59 +0200

    Four rectangles in loop with random rgb color fill

--- week-02/day-03/07-four-rectangles.ts
+++ week-02/day-03/07-four-rectangles.ts
@@ -13,15 +13,27 @@ let sideLengthA: number = 10;
 
 //calculators
 let sideLengthB: number = sideLengthA * 1.2;
+
+
+for (let r: number =0;r<= rectangleNr;r++) {
+    ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * r) + ',' + Math.floor(255 - 42.5 * r) + ',0)'
+//    ctx.fillStyle='blue';
+    ctx.fillRect(rectangleNr*(10+r*3),(rectangleNr+1)*(10*(r+3)),rectangleNr*(10*r),rectangleNr*(10/r));
+}
 //Function - Draw rectangle
-ctx.fillStyle = 'green';
+/*
 function drawRectangle(x: number, y: number) {
-    ctx.fillRect(x + (sideLengthA * 2 + 10), y-sideLengthB, sideLengthA, sideLengthB);
+    ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * x) + ',' + Math.floor(255 - 42.5 * y) + ',0)';
+ctx.fillRect(x*10,y*20,x,y)
+    //    ctx.fillRect(x + (sideLengthA * 2 + 10), y - sideLengthB, sideLengthA, sideLengthB);
+ctx.fillRect(x + (sideLengthA * 2 + 10), y - sideLengthB, sideLengthA+sideLengthA, sideLengthA+sideLengthB);
 }
-
-drawRectangle(10,20);
-/*for (let i: number = rectangleNr-1; i >= 0; i--) {
-    sideLengthA=sideLengthA+(sideLengthA*i);
-    drawRectangle(20+(30*i),40);
+for (let i: number = 0; i < rectangleNr; i++) {
+    drawRectangle(rectangleNr,sideLengthA);
+    rectangleNr=
 }
-
+/* for (let i: number = 0; i < rectangleNr; i++) {
+    drawRectangle(20 + (40 * i), 40);
+    sideLengthA = sideLengthA + (sideLengthA * (i + 5));
+}
+ */
