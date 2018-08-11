1ad58264afc73c13bccbc9bdaf33733035c05703
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-11 20:56:39 +0200

    side elements ready and scalable

--- week-01/day-04/22 - draw-square.ts
+++ week-01/day-04/22 - draw-square.ts
@@ -19,10 +19,30 @@ let lineCount: number = 6;
 // %----% 2-4
 // %%%%%% 6
 
-let repeatNr: number = 5;
-let star: string = '';
-for (let i: number = 0; i <=repeatNr;i++)
-{
-    star=star+'b';
-console.log(star);
+let brick: string = '';
+let space: string = '';
+
+//for (let i: number = 0; i <= lineCount; i++) {
+    //cycle for side lenght in this case lineCount
+    //if (i = 1) {
+        // first side creation no spaces needed
+        for (let j: number = 0; j <= lineCount-1; j++) {
+            // bricks increasing as lineCount
+            brick = brick + '%';
+        }
+        console.log(brick);
+        //i++;
+//}
+    //else {
+      //  for (let k: number = 0; k <= lineCount - 2; k++) { // inside and wall elements line number -2
+        //    if (k = 1) {
+                for (let l: number = 0; l <= lineCount -5; l++) {  // increasing space if lineCount inreased
+                    space = space + ' ';
+                }
+                console.log(brick.slice(0,1),space,brick.slice(0,1));
+                //i++;
+            /*}
+        }
+    }
 }
+*/
