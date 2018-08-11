672c96021f998552a842228ed2039f05fdf968ba
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-11 23:31:33 +0200

    scalable square ready

--- week-01/day-04/22 - draw-square.ts
+++ week-01/day-04/22 - draw-square.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-let lineCount: number = 6;
+let lineCount: number = 10;
 
 // Write a program that draws a square like this:
 //
@@ -22,27 +22,42 @@ let lineCount: number = 6;
 let brick: string = '';
 let space: string = '';
 
-//for (let i: number = 0; i <= lineCount; i++) {
+
+/*for (let i: number = 0; i <= lineCount; i++) {
     //cycle for side lenght in this case lineCount
-    //if (i = 1) {
+    if (i = 1) {
         // first side creation no spaces needed
-        for (let j: number = 0; j <= lineCount-1; j++) {
+        for (let j: number = 0; j <= lineCount - 1; j++) {
             // bricks increasing as lineCount
             brick = brick + '%';
         }
-        console.log(brick);
-        //i++;
-//}
-    //else {
-      //  for (let k: number = 0; k <= lineCount - 2; k++) { // inside and wall elements line number -2
-        //    if (k = 1) {
-                for (let l: number = 0; l <= lineCount -5; l++) {  // increasing space if lineCount inreased
-                    space = space + ' ';
-                }
-                console.log(brick.slice(0,1),space,brick.slice(0,1));
-                //i++;
-            /*}
-        }
+        console.log('1stline', brick, );
     }
+    else {
+        //for (let k: number = 0; k <= lineCount - 2; k++) { // inside and wall elements line number -2
+        //    if (k = 1) {
+        for (let l: number = 0; l <= lineCount - 5; l++) {  // increasing space if lineCount increased
+            space = space + ' ';
+        }
+        console.log(brick.slice(0, 1), space, brick.slice(0, 1));
+        //k++;
+    }
+}*/
+
+for (let k: number = 0; k <= lineCount - 1; k++) {
+    // bricks increasing as lineCount
+    brick = brick + '%';
 }
-*/
+console.log(brick);
+for (let l: number = 0; l <= lineCount - 5; l++) {  // increasing space if lineCount increased
+    space = space + ' ';
+}
+for (let m: number = 0; m < lineCount - 2; m++) {
+    console.log(brick.slice(0, 1), space, brick.slice(0, 1));
+}
+brick = brick.slice(0, 1);
+for (let n: number = 0; n <= lineCount - 2; n++) {
+    // bricks increasing as lineCount
+    brick = brick + '%';
+}
+console.log(brick);
