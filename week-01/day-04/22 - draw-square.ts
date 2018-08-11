fdeeb50229e688899b05e61c35e093c13c481034
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-11 23:40:23 +0200

    comments added

--- week-01/day-04/22 - draw-square.ts
+++ week-01/day-04/22 - draw-square.ts
@@ -22,7 +22,7 @@ let lineCount: number = 10;
 let brick: string = '';
 let space: string = '';
 
-// bricks increasing as lineCount
+// bricks increasing as lineCount - first line
 for (let k: number = 0; k <= lineCount - 1; k++) {
     brick = brick + '%';
 }
@@ -34,7 +34,7 @@ for (let l: number = 0; l <= lineCount - 5; l++) {
 for (let m: number = 0; m < lineCount - 2; m++) {
     console.log(brick.slice(0, 1), space, brick.slice(0, 1));
 }
-// bricks increasing as lineCount
+// bricks increasing as lineCount - last line
 brick = brick.slice(0, 1); //reset the state of the bricks
 for (let n: number = 0; n <= lineCount - 2; n++) {
 
