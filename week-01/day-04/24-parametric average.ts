a17c6b40e762507634cf66d986a76fd175d0dc55
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-12 11:11:56 +0200

    random number generator function added

--- week-01/day-04/24-parametric average.ts
+++ week-01/day-04/24-parametric average.ts
@@ -8,18 +8,29 @@
 // Sum: 22, Average: 4.4
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-let yourNumber: number=10;
-let sum: number =0;
+let yourNumber: number = 2;
+let sum: number = 0;
+let min: number = 0;
+let max: number = 10;
 let randomNr: number = 0;
-randomNr=Math.random();
-for (let i: number;i<=yourNumber;i++) {
-randomNr=Math.random();
-sum=sum+randomNr;
-}
-let avarageValue: number=sum/yourNumber;
+//randomNr = Math.random();
+//console.log('randomNr',randomNr);
+//console.log('sum',sum);
 
-console.log('The summariyed value of the random numbers: ',sum)
-console.log('The avarage value of the ',yourNumber,'randomized numbers: ',sum/yourNumber,avarageValue)
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+for (let i: number=0; i < yourNumber; i++) {
+    randomNr=getRandomInt(max);
+    //console.log('random number generated',randomNr);
+    sum = sum + randomNr;
+    //console.log('summofnumbers',sum);
+}
+
+console.log('The summarized value of the random numbers: ', sum);
+console.log('The avarage value of the ', yourNumber, 'randomized numbers: ', sum / yourNumber);
 /* {Math.random(
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
@@ -36,4 +47,4 @@ console.log('The avarage value of the ',yourNumber,'randomized numbers: ',sum/yo
       
 )
 
-} */
+}*/
