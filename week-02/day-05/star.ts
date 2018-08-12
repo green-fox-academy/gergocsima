20a8547c7316f92dcf6aa2d0f226469c517238f5
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-12 21:00:22 +0200

    quarters ready and scalable

--- week-02/day-05/star.ts
+++ week-02/day-05/star.ts
@@ -6,7 +6,7 @@ let scaleDownRation: number = 2;
 let positioning: number =200;
 let canvasWidth: number = canvas.width / scaleDownRation;
 let canvasHeight: number = canvas.height / scaleDownRation;
-let lineNumber: number = 20;
+let lineNumber: number = 40;
 let startY: number = 0+positioning;
 let startX: number = canvasWidth / lineNumber;
 let endY: number = canvasHeight / lineNumber;
@@ -16,9 +16,13 @@ let desiredColour1: string = 'cyan';
 let addedColour1 = [desiredColour1];
 let desiredColour2: string = 'orange';
 let addedColour2 = [desiredColour2];
+let desiredColour3: string = 'cyan';
+let addedColour3 = [desiredColour3];
+let desiredColour4: string = 'orange';
+let addedColour4 = [desiredColour4];
 
-//draw lines into left corner
-function drawLine(lineNumber) {
+//3rd quarter of the star
+function drawLine3rdQ(lineNumber) {
     for (let i: number = 1; i < lineNumber; i++) {
         ctx.beginPath();
         ctx.strokeStyle = addedColour1;
@@ -29,4 +33,47 @@ function drawLine(lineNumber) {
 
     }
 }
-drawLine(lineNumber);
+drawLine3rdQ(lineNumber);
+
+//3rd quarter of the star
+function drawLine4thQ(lineNumber) {
+    for (let i: number = 1; i < lineNumber; i++) {
+        ctx.beginPath();
+        ctx.strokeStyle = addedColour1;
+        ctx.moveTo(startX * i, startY);
+        ctx.lineTo(canvasWidth, positioning-endY * i);
+        ctx.stroke();
+        ctx.closePath();
+
+    }
+}
+drawLine4thQ(lineNumber);
+
+
+//2nd quarter of the star
+function drawLine2ndQ(lineNumber) {
+    for (let i: number = 1; i <= lineNumber; i++) {
+        ctx.beginPath();
+        ctx.strokeStyle = addedColour2;
+        ctx.moveTo(2*canvasWidth-startX*i, startY);
+        ctx.lineTo(canvasWidth, positioning-endY *i);
+        ctx.stroke();
+        ctx.closePath();
+
+    }
+}
+drawLine2ndQ(lineNumber);
+
+//2nd quarter of the star
+function drawLine1stQ(lineNumber) {
+    for (let i: number = 1; i < lineNumber; i++) {
+        ctx.beginPath();
+        ctx.strokeStyle = addedColour2;
+        ctx.moveTo(canvasWidth, endY * i+positioning);
+        ctx.lineTo(2*canvasWidth-startX*i, startY);
+        ctx.stroke();
+        ctx.closePath();
+
+    }
+}
+drawLine1stQ(lineNumber);
