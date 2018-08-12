ad86ec4ce701dfad7fcaa01c5d1481e949a35eac
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-12 23:46:03 +0200

    all array elements halfed

---
+++ week-02/day-01/12-change-element.ts
@@ -0,0 +1,6 @@
+// -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
+let numList = [1, 2, 3, 8, 5, 6];
+numList= numList.map(x=>x/2);
+console.log(numList[3]);
