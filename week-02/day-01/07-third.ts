3ce0b728675a6247bead770c5053dfc0a6a0879d
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-12 21:35:07 +0200

    var changed to let

--- week-02/day-01/07-third.ts
+++ week-02/day-01/07-third.ts
@@ -3,11 +3,11 @@
 // -  Print the third element of `magicNumbers`
 //
 
-var magicNumbers = [1, 3, 5, 7];
+let magicNumbers = [1, 3, 5, 7];
 console.log(magicNumbers[2]); //using 2 variables
 
-var startIndex=2;
-var numberOfElementsToRemove = 1;
-var removedElemets =magicNumbers.splice(startIndex,numberOfElementsToRemove);
+let startIndex=2;
+let numberOfElementsToRemove = 1;
+let removedElemets =magicNumbers.splice(startIndex,numberOfElementsToRemove);
 console.log(removedElemets);
 
