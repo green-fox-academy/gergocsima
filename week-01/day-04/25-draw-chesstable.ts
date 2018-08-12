e7ffebe18d2f5d9981e48d736ca2068849279bfe
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-12 11:34:25 +0200

    desired chess table added

---
+++ week-01/day-04/25-draw-chesstable.ts
@@ -0,0 +1,40 @@
+'use strict';
+
+// Create a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
+
+let lineCount: number = 10;
+let brick: string = '%';
+let space: string = ' ';
+let lineA: string = '';
+let lineB: string = '';
+
+
+for (let i: number = 0; i < lineCount / 2; i++) {
+    lineA = lineA + space + brick;
+
+    //line = line + brick + space;
+    //console.log(line);
+} //console.log(lineA);
+for (let k: number = 0; k < lineCount / 2; k++) {
+    lineB = lineB + brick + space;
+
+    //line = line + brick + space;
+    //console.log(line);
+} //console.log(lineB);
+
+for (let j: number = 0; j <= lineCount - 1; j++) {
+    if (j % 2 == 0) {
+        console.log(lineB);
+    } else { console.log(lineA); }
+}
+
