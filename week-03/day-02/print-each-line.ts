ef6410b558dae11ee752a798e9bbfee4286eab53
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-14 14:22:10 +0200

    function created, result ok +undefined

--- week-03/day-02/print-each-line.ts
+++ week-03/day-02/print-each-line.ts
@@ -2,3 +2,19 @@
 // each of lines form the file.
 // If the program is unable to read the file (for example it does not exists),
 // then it should print an error message like: 'Unable to read file: my-file.txt'
+
+'use strict';
+declare function require(path: string): any;
+export { };
+
+const charEncoding = 'utf-8';
+const fs = require('fs');
+function readLines(x: string) {
+  try {
+    return fs.readFileSync(x, charEncoding);
+  } catch (error) {
+console.log('Unable to read file:'+x)
+  }
+}
+console.log(readLines('my-fil.txt'));
+
