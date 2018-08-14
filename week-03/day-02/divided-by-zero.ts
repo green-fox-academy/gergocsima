3dcc7d3d03ac8d00823aabd22db851628ea2b669
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-14 12:18:45 +0200

    function for division

---
+++ week-03/day-02/divided-by-zero.ts
@@ -0,0 +1,13 @@
+// Create a function that takes a number
+// divides ten with it,
+// and prints the result.
+// It should print 'fail' if the parameter is 0
+
+let input: number = 10;
+
+function divider(x: number):number { 
+let result=10/x;
+console.log(result);
+}
+
+divider(input);
