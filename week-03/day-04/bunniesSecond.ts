4fb3385f401873cea73af6db173c6e3a1174dddb
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-16 14:46:27 +0200

    recursion conditinal order corrected

--- week-03/day-04/bunniesSecond.ts
+++ week-03/day-04/bunniesSecond.ts
@@ -2,16 +2,20 @@
 // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
 // have 3 ears, because they each have a raised foot. Recursively return the
 // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+export { }
 
-let bunnyNumber: number = 10;
+let oddears: number = 0;
+let nonodd: number = 0;
+let earNumber: number = oddears + nonodd;
 
-function countEars(bunnyNumber) {
-  if (bunnyNumber%2==0) {
-    return 3+bunnyEars(bunnyNumber-1);
+function countEars(bunnyNumber: number) {
+  if (bunnyNumber == 0) {
+    return 0;
   }
-  else if (bunnyNumber%2!==0) {
-    return 1+bunnyEars(bunnyNumber-1)
-  } else{
-    return 0
-  }
+  else if (bunnyNumber % 2 !== 0) {
+    return 2 + countEars(bunnyNumber - 1);
+  } else {
+    return 3 + countEars(bunnyNumber - 1);
 }
+}
+console.log(countEars(3));
