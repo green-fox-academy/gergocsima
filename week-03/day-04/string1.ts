a75d2e8e7e687829423274ce0dfaa51d8c1aa9c0
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-16 14:51:43 +0200

    file created

---
+++ week-03/day-04/string1.ts
@@ -0,0 +1,10 @@
+// Given a string, compute recursively (no loops) a new string where all the
+// lowercase 'x' chars have been changed to 'y' chars.
+
+let givenString: string = 'XXXyxxxzzggsjavdvxv';
+
+function xToY(text:string) {
+  if (givenString.indexOf==-1) {
+    return 0;
+  }
+}
