adf8094d14838d7006b93c7030ff397a5beb1760
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-16 13:52:38 +0200

    if conditional corrected, recursive function in effect

--- week-03/day-04/powder.ts
+++ week-03/day-04/powder.ts
@@ -2,10 +2,10 @@
 // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
 
 function powerN(base: number, power: number) {
-if (power=0) {
-return 1;
-} else {
-  return base*powerN(base,power-1);
+  if (power== 0) {
+    return 1;
+  } else {
+    return base * powerN(base,(power - 1));
+  }
 }
-}
-console.log(powerN(3,2));
+console.log(powerN(3,4));
