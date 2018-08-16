0e58c1f4487c0afb4f6a7de4c1de1a8ecd3d3cd4
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-16 16:15:31 +0200

    recursion added

--- week-03/day-04/string1.ts
+++ week-03/day-04/string1.ts
@@ -2,9 +2,16 @@
 // lowercase 'x' chars have been changed to 'y' chars.
 
 let givenString: string = 'XXXyxxxzzggsjavdvxv';
+//console.log(givenString.indexOf('x',givenString.length));
+// console.log(givenString.replace('x','y'));
 
-function xToY(text:string) {
-  if (givenString.indexOf==-1) {
-    return 0;
+function xToY(text: string): string {
+  if (text.indexOf('x')== -1) {
+    return text;
+  }
+  else {
+    text = text.replace('x', 'y');
+    return xToY(text);
   }
 }
+console.log(xToY(givenString));
