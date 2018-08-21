54c3c888c8107fb11e91df17474161c1f2b6e1d1
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-21 15:37:44 +0200

    (feat): removed test parameters

---
+++ week-04/day-01/programGFA.ts
@@ -0,0 +1,5 @@
+import { StudentGFA } from "./gfa02";
+import { MentorGFA } from "./gfa03";
+import { PersonGFA } from "./gfa01";
+import { SponsorGFA } from "./gfa04";
+import { Cohort } from "./gfa05";
