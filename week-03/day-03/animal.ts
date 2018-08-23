25f80b02ebc00a87a018d73953a45444686a37bb
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-23 23:26:29 +0200

    function definitions corrected

--- week-03/day-03/animal.ts
+++ week-03/day-03/animal.ts
@@ -1,16 +1,24 @@
+// Create an Animal class
+// Every animal has a hunger value, which is a whole number
+// Every animal has a thirst value, which is a whole number
+// when creating a new animal object these values are created with the default 50 value
+// Every animal can eat() which decreases their hunger by one
+// Every animal can drink() which decreases their thirst by one
+// Every animal can play() which increases both by one
+
 export class Animal {
-  protected hungerValue: number = 0;
-  protected thirstValue: number = 0;
+  protected hungerValue: number;
+  protected thirstValue: number;
 
   constructor(hungerValue: number = 50, thirstValue: number = 50) {
     this.hungerValue = hungerValue;
     this.thirstValue = thirstValue;
   }
   eat() {
-    return this.thirstValue--;
+    return this.hungerValue--;
   }
   drink() {
-    return this.hungerValue--;
+    return this.thirstValue--;
   }
   play() {
     return this.hungerValue++ , this.thirstValue++;
