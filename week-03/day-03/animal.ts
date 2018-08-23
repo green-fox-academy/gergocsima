48e5762c5f99a0e8ae8efb1eeec696b52fafd942
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-08-23 22:28:49 +0200

    protected access added

--- week-03/day-03/animal.ts
+++ week-03/day-03/animal.ts
@@ -1,31 +1,23 @@
-/* Create an Animal class
-Every animal has a hunger value, which is a whole number
-Every animal has a thirst value, which is a whole number
-when creating a new animal object these values are created with the default 50 value
-Every animal can eat() which decreases their hunger by one
-Every animal can drink() which decreases their thirst by one
-Every animal can play() which increases both by one */
-
 export class Animal {
-  hungerValue: number = 0;
-  thirstValue: number = 0;
+  protected hungerValue: number = 0;
+  protected thirstValue: number = 0;
 
-  constructor (hungerValue: number = 50, thirstValue: number = 50) {
+  constructor(hungerValue: number = 50, thirstValue: number = 50) {
     this.hungerValue = hungerValue;
     this.thirstValue = thirstValue;
   }
-  eat(){
+  eat() {
     return this.thirstValue--;
   }
-  drink(){
+  drink() {
     return this.hungerValue--;
   }
-  play(){
-    return this.hungerValue++,this.thirstValue++;
+  play() {
+    return this.hungerValue++ , this.thirstValue++;
   }
-getHungry(){
-return  this.hungerValue;
-}
+  getHungry() {
+    return this.hungerValue;
+  }
 }
 
 let Animal1: Animal = new Animal();
