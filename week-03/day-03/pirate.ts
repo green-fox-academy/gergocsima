9c0912c088237ec70f3962fab7baecdca0ebde4f
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-09-04 12:37:00 +0200

    (feat) brawl working fine

--- week-03/day-03/pirate.ts
+++ week-03/day-03/pirate.ts
@@ -2,56 +2,59 @@ export class Pirate {
   name: string;
   alcLevel: number;
   status: boolean;
-  constructor(naem: string, alcLevel: number = 0, status: boolean = false) {
+  constructor(name: string, alcLevel: number = 0, status: boolean = true) {
     this.name = name;
     this.alcLevel = alcLevel;
     this.status = status;
   }
   drinSomeRum() {
-    if (this.status == false) {
+    if (this.status == true) {
       return this.alcLevel++;
     } else {
-      return `I'am dead mate...!`
+      return `${this.name}:I'am dead mate...!`
     }
   }
   howsItGoingMate() {
     if (this.alcLevel <= 4) {
-      return "Pour me anudder!";
+      return `${this.name}: Pour me anudder!`;
     } else {
       this.alcLevel = 0;
-      return "Arghh, I'ma Pirate. How d'ya d'ink its goin?"
-
+      return `${this.name}:Arghh, I'ma Pirate. How d'ya d'ink its goin?`
     }
   }
   die() {
-    if (this.status == false) {
-      return true
+    if (this.status == true) {
+      this.status = false;
+      return `${this.name}: OOOohh I am deeead....`
     }
   }
   brawl(piaretOpp: Pirate) {
-    if (this.status != true && piaretOpp.status != true) {
-      let chance = Math.floor(Math.random() * 3)
-      if (chance = 1) {
-        piaretOpp.die()
-      } else if (chance = 2) {
-        this.die()
-      } else {
-        this.alcLevel = 4;
-        piaretOpp.alcLevel = 4;
-      }
+    let chance: number = Math.floor(Math.random() * 3);
+    if (piaretOpp.status != false && chance === 1) {
+      piaretOpp.die();
+      return `${piaretOpp.name} died`
+    } else if (piaretOpp.status != false && chance === 2) {
+      this.die();
+      return `${this.name} died`
+    } else {
+      this.alcLevel = 4;
+      piaretOpp.alcLevel = 4;
+      return `${this.name} and ${piaretOpp.name} We both passed outh mate...`
     }
   }
 }
-// Create a Pirate class. While you can add other fields and methods, you must have these methods:-
 
-// drinkSomeRum() - intoxicates the Pirate some
-// howsItGoingMate() - when called, the Pirate replies, if drinkSomeRun was called:-
-// 0 to 4 times, "Pour me anudder!"
-// else, "Arghh, I'ma Pirate. How d'ya d'ink its goin?", the pirate passes out and sleeps it off.
-// If you manage to get this far, then you can try to do the following. - die() - this kills off the pirate,
-// in which case, DrinkSomeRum, etc. just result in he's dead.
-// - brawl(x) - where pirate fights another pirate (if that other pirate is alive) and there's a 1/3 chance, 
-//1 dies, the other dies or they both pass out.
+let laci: Pirate = new Pirate('Laci');
+let bela: Pirate = new Pirate('Bela', 3);
+let jozso: Pirate = new Pirate('Jozso', 2);
+console.log(laci.alcLevel);
+laci.drinSomeRum();
+laci.drinSomeRum();
+console.log(laci.howsItGoingMate());
+console.log(laci.alcLevel);
+jozso.die();
+console.log(laci.drinSomeRum());
+console.log(laci.brawl(bela));
 
 // And... if you get that far...
 
