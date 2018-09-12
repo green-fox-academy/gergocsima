5cfb30bcb90f77e5aadce0709580193195e8afaf
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-09-12 11:24:43 +0200

    (feat) .gitignore updated

---
+++ week-06/day-02/deallocatemem/src/deallocatemem.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// with malloc
+// please allocate a 10 long array and fill it with even numbers then print the whole array
+// please deallocate memory without using free
+
+int main()
+{
+	int *pointer = NULL;
+
+	return 0;
+}
