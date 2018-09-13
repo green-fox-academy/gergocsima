417cbd93afb8e67b32e59e29111c844957b3c031
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-09-13 11:42:37 +0200

    (func) file created and the text readed in function

--- week-06/day-04/print_lines.c
+++ week-06/day-04/print_lines.c
@@ -9,6 +9,23 @@ int main()
     FILE *fptr;
     fptr = fopen("my-file.txt", "w");
 
-    fprintf(fptr, "This is a testing file for file writing and reading in c.\n");
+    fprintf(fptr, "This is a testing file for file writing and reading in c second.\n");
     fclose(fptr);
+    char c[1000];
+    FILE *fptr2;
+
+    if ((fptr2 = fopen("my-file.txt", "r")) == NULL)
+    {
+        printf("Error! opening file");
+        // Program exits if file pointer returns NULL.
+        exit(1);
+    }
+
+    // reads text until newline
+    fscanf(fptr, "%[^\n]", c);
+
+    printf("Data from the file:\n%s", c);
+    fclose(fptr);
+
+    return 0;
 }
