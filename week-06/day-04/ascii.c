60b719fb5755ef4c29ac4136cc6d04e6a211fd0b
Gergely.Csima <gergely.csima@gmail.comgit config --global core.editor emacsgit config --global user.email gergely.csima@gmail.com>
2018-09-13 15:54:15 +0200

    (feat) ASCIII translation to letters

---
+++ week-06/day-04/ascii.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// print out the ascii values of the array
+
+int main()
+{
+    int array[] = {103, 114, 101, 101, 110, 32, 102, 111, 120};
+    int size = sizeof(array) / sizeof(int);
+    printf("The translation of ASCII :");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%c", array[i]);
+    }
+    return 0;
+}
