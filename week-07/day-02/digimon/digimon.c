b2f8bc1237216a3e92aa4e5c613426ed63980361
Gergely.Csima <gergely.csima@gmail.com>
2018-09-18 19:55:09 +0200

    (feat) exeption handling added

--- week-07/day-02/digimon/digimon.c
+++ week-07/day-02/digimon/digimon.c
@@ -13,52 +13,80 @@
 int minimumHealth(digim_t *array, int array_length)
 {
     int current = 100;
-    for (int i = 0; i < array_length; i++)
+    if (array == NULL)
     {
-        if (array[i].health < current)
-        {
-            current = array[i].health;
-        }
+        printf("The digimon list you checked is empty");
     }
-    return current;
+    else
+    {
+        for (int i = 0; i < array_length; i++)
+        {
+            if (array[i].health < current)
+            {
+                current = array[i].health;
+            }
+        }
+        return current;
+    }
 }
 
 int digivolLevelCount(digim_t *array, int array_length, enum digimonLevel digi_t)
 {
     int counter = 0;
-    for (int i = 0; i < array_length; i++)
+    if (array == NULL)
     {
-        if (array[i].level == digi_t)
-        {
-            counter++;
-        }
+        printf("The digimon list you checked is empty");
     }
-    return counter;
+    else
+    {
+        for (int i = 0; i < array_length; i++)
+        {
+            if (array[i].level == digi_t)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
 int sameTamer(digim_t *array, int array_length, char *tamer)
 {
     int counter = 0;
-
-    for (int i = 0; i < array_length; i++)
+    if (array == NULL)
     {
-        if (strcmp(array[i].tamerName, tamer) == 0)
-        {
-            counter++;
-        }
+        printf("The digimon list you checked is empty");
     }
-    return counter;
+    else
+    {
+
+        for (int i = 0; i < array_length; i++)
+        {
+            if (strcmp(array[i].tamerName, tamer) == 0)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
 int avarageHealth(digim_t *array, int array_length, char *tamer)
 {
     int sumHealth = 0;
     int numOfDigis = 0;
-    for (int j = 0; j < array_length; j++)
+    if (array == NULL)
     {
-        if (strcmp(array[j].tamerName, tamer) == 0)
-        {
-            sumHealth += array[j].health;
-            numOfDigis++;
-        }
+        printf("The digimon list you checked is empty");
     }
-    return sumHealth / numOfDigis;
+    else
+    {
+        for (int j = 0; j < array_length; j++)
+        {
+            if (strcmp(array[j].tamerName, tamer) == 0)
+            {
+                sumHealth += array[j].health;
+                numOfDigis++;
+            }
+        }
+        return sumHealth / numOfDigis;
+    }
 }
