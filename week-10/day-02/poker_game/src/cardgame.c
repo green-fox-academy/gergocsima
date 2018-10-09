40897cd35296af5e9904c0b385efacab328e6c09
Gergely.Csima <gergely.csima@gmail.com>
2018-10-10 00:03:14 +0200

    includes added

---
+++ week-10/day-02/poker_game/src/cardgame.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include "cardgame.h"
+
+/*
+read_hands_from_file(filename); -> return array of names with their hands and size
+   read_number_of_cards(); -> returns the number that was entered
+   generate_random_cards(number, random_cards); -> fills random_cards array with 'number' random cards
+   decide_winner(random_cards, array of names with hands and size); -> returns a name
+*/
