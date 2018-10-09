331f7e281702d04b60e56d7c1ca755e69d3a1be4
Gergely.Csima <gergely.csima@gmail.com>
2018-10-10 00:02:23 +0200

    struct created

--- week-10/day-02/poker_game/include/cardgame.h
+++ week-10/day-02/poker_game/include/cardgame.h
@@ -17,3 +17,36 @@ typedef enum
     HEART,
     SPADE
 } suits_t;
+typedef enum
+{
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+} deck_t;
+
+typedef struct players
+{
+    char name[100];
+    suits_t suit_one;
+    deck_t card_one;
+    suits_t suit_two;
+    deck_t card_two;
+    int points;
+} player_t;
+
+typedef struct card
+{
+    suits_t suit;
+    deck_t deck;
+    state_t status;
+} card_t;
